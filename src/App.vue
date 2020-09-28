9b8c734462324e5c0e99a69710e20ab52e5527b5
aslawny <alexis.slawny@gmail.com>
2020-09-28 13:12:24 +0200

    second commit test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Bonjour Le Coding en vue" />
+  <img alt="just logo" src="./assets/image-test.png" />
 </template>
 
 <script>
