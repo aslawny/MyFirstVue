5215bb99dfcd8462efb5a41e442dce159a2f0e62
aslawny <alexis.slawny@gmail.com>
2020-09-28 14:13:23 +0200

    ajoute menu header (marche pas encore)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Bonjour Le Coding en vue" />
   <img alt="just logo" src="./assets/image-test.png" />
+  <HelloWorld msg="Bonjour Le Coding en vue" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
