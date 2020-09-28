8af8a821906e11dc1eaa9ef2a6f5d143b040df73
aslawny <alexis.slawny@gmail.com>
2020-09-28 12:56:40 +0200

    init first vue js app for teaching

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Bonjour Le Coding en vue" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
